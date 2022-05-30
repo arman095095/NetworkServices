b3b5cb4a4053eb6297e7db208cdbf08927642291
Арман Чархчян <re-mont1@mail.ru>
2022-05-30 22:44:37 +0400

    fix

--- Sources/NetworkServices/Info/FirebaseNetworkServiceAdapter.swift
+++ Sources/NetworkServices/Info/FirebaseNetworkServiceAdapter.swift
@@ -7,25 +7,67 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
 
 public protocol NetworkServiceProtocol {
     func dataTask(with request: NetworkRequest, completion: @escaping (Result<Data, Error>) -> ())
     func collectionSocketTask(with request: NetworkSocketCollectionRequest, completion: @escaping (Result<(added: Data, removed: Data, edited: Data), Error>) -> ()) -> SocketProtocol
     func socketDocumentTask(with request: NetworkSocketDocumentRequest, completion: @escaping (Result<Data, Error>) -> ()) -> SocketProtocol
+    func downloadTask(request: LoadRequest, completion: @escaping (Result<Data, Error>) -> ())
+    func uploadTask(request: LoadRequest, data: DataWrapper, completion: @escaping (Result<URL, Error>) -> ())
+    func deleteData(request: LoadRequest)
 }
 
 public final class FirebaseNetworkServiceAdapter {
-    private let baseURL: Firestore
+    private let baseFirestoreURL: Firestore
+    private let baseStorageURL: Storage
+    private let baseAuthURL: Auth
     
-    init(firestore: Firestore) {
-        self.baseURL = firestore
+    init(baseFirestoreURL: Firestore, baseStorageURL: Storage, baseAuthURL: Auth) {
+        self.baseFirestoreURL = baseFirestoreURL
+        self.baseStorageURL = baseStorageURL
+        self.baseAuthURL = baseAuthURL
     }
 }
 
 extension FirebaseNetworkServiceAdapter: NetworkServiceProtocol {
+    public func downloadTask(request: LoadRequest, completion: @escaping (Result<Data, Error>) -> ()) {
+        let endPoint = baseStorageURL.reference(forURL: request.path)
+        let megaByte = Int64(1*1024*1024)
+        endPoint.getData(maxSize: megaByte) { data, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else { return }
+            completion(.success(data))
+        }
+    }
+    
+    public func uploadTask(request: LoadRequest, data: DataWrapper, completion: @escaping (Result<URL, Error>) -> ()) {
+        baseStorageURL.reference(forURL: request.path).putData(data.value, metadata: data.metadata) { (metadata, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            self.baseStorageURL.reference(forURL: request.path).downloadURL { (url, error) in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                guard let downloadURL = url else { return }
+                completion(.success(downloadURL))
+            }
+        }
+    }
+    
+    public func deleteData(request: LoadRequest) {
+        baseStorageURL.reference(forURL: request.path).delete()
+    }
     
     public func dataTask(with request: NetworkRequest, completion: @escaping (Result<Data, Error>) -> ()) {
-        let endPoint = baseURL.collection(request.path)
+        let endPoint = baseFirestoreURL.collection(request.path)
         switch request.httpMethod {
         case .get:
             getDataTask(endPoint: endPoint, completion: completion)
@@ -53,7 +95,7 @@ extension FirebaseNetworkServiceAdapter: NetworkServiceProtocol {
 
 extension FirebaseNetworkServiceAdapter {
     public func collectionSocketTask(with request: NetworkSocketCollectionRequest, completion: @escaping (Result<(added: Data, removed: Data, edited: Data), Error>) -> ()) -> SocketProtocol {
-        let endPoint = baseURL.collection(request.path)
+        let endPoint = baseFirestoreURL.collection(request.path)
         let listener = endPoint.addSnapshotListener { query, error in
             if let error = error {
                 completion(.failure(error))
@@ -85,7 +127,7 @@ extension FirebaseNetworkServiceAdapter {
     }
     
     public func socketDocumentTask(with request: NetworkSocketDocumentRequest, completion: @escaping (Result<Data, Error>) -> ()) -> SocketProtocol {
-        let endPoint = baseURL.collection(request.path)
+        let endPoint = baseFirestoreURL.collection(request.path)
         let listener = endPoint.document(request.documentID).addSnapshotListener { snapshot, error in
             if let error = error {
                 completion(.failure(error))
---
+++ Sources/NetworkServices/Other/LastDocument.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Арман Чархчян on 30.05.2022.
+//
+
+import Foundation
+import FirebaseFirestore
+
+public struct LastDocument<T> {
+    var document: T
+}
